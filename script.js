b96a83e51ed7b4e2e521f7dc23c683f307b6b9ed
ImShubham09 <singhashubham612@gmail.com>
2024-12-23 22:10:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this._make = make;
+	this._model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+getMakeModel(){
+	return `${this.make} ${this.model}`;
+}
+
+function SportsCar(make, model, topSpeed) {
+	super();
+	this._topSpeed = topSpeed;
+}
+
+getTopSpeed(){
+	return `${this.make} ${this.model} ${this.topSpeed}`;
+}
 
 // Do not change the code below
 window.Car = Car;
